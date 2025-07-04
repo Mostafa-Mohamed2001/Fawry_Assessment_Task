8db7f23c75e9f2452b1eff7bc599712291e588b1
Mostafa Mohamed <Mostafa2001Mohamed2001@gmail.com>
2025-07-04 23:22:41 +0300

    Update Program.cs

--- FawryChallenge/Program.cs
+++ FawryChallenge/Program.cs
@@ -9,15 +9,21 @@ namespace FawryChallenge
         static void Main(string[] args)
         {
             
-            Customer customer = new Customer() { Name="Mostafa",Balance=1000};
-            
-            Product cheese = new Product() { Name="Cheese",Price=225,ProductQuantity=2,IsShipabble=true,Weight=420};
-            Product biscuit = new Product() { Name= "Biscuit", Price=130,ProductQuantity=7, IsShipabble = true, Weight = 250 };
-            Product tv = new Product() { Name= "TV", Price=317,ProductQuantity=16,IsShipabble=false,Weight=0};
-            Cart cart = new Cart();
-            cart.Add(cheese,2);
-            cart.Add(biscuit,1);
-            cart.Add(tv,1);
+             Customer customer = new Customer() { Name="Mostafa",Balance=1000};
+
+                //shipable and may have an expiration date
+                Product cheese = new Product() { Name="Cheese",Price=225,ProductQuantity=2,IsShipabble=true,Weight=420};
+                Product biscuit = new Product() { 
+                    Name = "Biscuit", Price = 130, ProductQuantity = 7, IsShipabble = true, Weight = 250, //ExpirationDate= DateTime.Now.AddDays(1)
+                };
+                // not shipable and not expirable
+                Product pen = new Product() { Name= "pen", Price=317,ProductQuantity=16,IsShipabble=false,Weight=0};
+                Product scratchCard = new Product() { Name= "scratchCard", Price=10,ProductQuantity=16,Weight=0};
+                Cart cart = new Cart();
+                cart.Add(cheese,2);
+                cart.Add(biscuit,1);
+                cart.Add(pen, 1);
+                cart.Add(scratchCard, 1);
 
             ShippingService shippingService = new ShippingService();
             shippingService.ShapingProducts(cart);
